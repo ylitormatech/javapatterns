45b9e9cab1e0733c268371868f593dc50a8666ee
toprat <marco@ylitorma.com>
2016-03-26 19:17:54 +0200

    observer test inprogress

--- src/javapatterns/observable/User.java
+++ src/javapatterns/observable/User.java
@@ -12,19 +12,25 @@ package javapatterns.observable;
 public class User implements Observer {
     private Observable observable = null;
     
+    private boolean inStock = false;
+    
     public User(Observable observable) {
         this.observable = observable;
     }
     
     @Override
     public void update() {
-        
+        buyCar();
+        unsubscribe();
     }
     
     public void buyCar() {
         System.out.println("Ostin uuden auton");
     }
     
+    public void subscribe() {
+        observable.addObserver(this);
+    }
     public void unsubscribe() {
         observable.removeObserver(this);
     }
